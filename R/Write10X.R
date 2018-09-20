addd544c59e963a6da00fc2722e4cf421a5ba4d8
Dan Skelly <daskelly@users.noreply.github.com>
2018-09-20 13:29:26 -0400

    initial commit of package of add-on functions for Seurat

---
+++ R/Write10X.R
@@ -0,0 +1,19 @@
+#' Write data in 10X format i.e. directory with barcodes.tsv, genes.tsv, matrix.mtx
+#'
+#' @export
+#' @examples
+#' Write10X(obj, dir)
+Write10X <- function(obj, dir) {
+    assertthat::assert_that(class(obj) == "seurat")
+    
+    # Barcodes
+    cat(obj@cell.names, file = paste0(dir, "/barcodes.tsv"), sep = "\n")
+    
+    # Genes. Seurat doesn't store ENS ID. As a hack, put ENS_ID for all genes
+    df <- data.frame(ID = "ENS_ID", symbol = rownames(obj@raw.data))
+    write.table(df, row.names = F, col.names = F, sep = "\t", quote = F, file = paste0(dir, "/genes.tsv"))
+    
+    # Matrix package is imported by Seurat
+    mat <- obj@raw.data[, obj@cell.names]
+    writeMM(mat, file = paste0(dir, "/matrix.mtx"))
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# Signac -- extension functions to Seurat
+
+Here are two helpful links that I used when building
+this package:
+ 1. https://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/
+ 2. http://kbroman.org/pkg_primer/
+
+```r
+library(devtools)
+library(roxygen2)
+
+setwd("~/repos")
+create("signac")
+# manually add functions ...
+# ...
+devtools::use_package("Seurat", "Imports")
+devtools::use_package("assertthat", "Imports")
+setwd('./signac')
+document()
+```
+
+Two easy methods to keep code tidy and well-formatted:
+```r
+formatR::tidy_dir("R")
+lintr::lint_package()
+```
+See [here](http://r-pkgs.had.co.nz/r.html).
