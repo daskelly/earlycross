c27400f6298df2e6525de9bcfc634f6c4d956833
Dan Skelly <daskelly@users.noreply.github.com>
2018-09-20 15:26:56 -0400

    add gzip functionality to WriteTsv()

--- R/WriteTsv.R
+++ R/WriteTsv.R
@@ -3,10 +3,11 @@
 #' @param obj Seurat object to print
 #' @param outfile Character. Name of a tsv file that should be written to
 #' @param raw A logical scalar. Should raw data be written?
+#' @param gzip A logical scalar. Should data be gzipped after writing?
 #' @export
 #' @examples
 #' WriteTsv(obj, filename, raw=FALSE)
-WriteTsv <- function(obj, outfile, raw=TRUE) {
+WriteTsv <- function(obj, outfile, raw=TRUE, gzip=TRUE) {
     assertthat::assert_that(class(obj) == "seurat")
     
     if (raw) {
@@ -17,5 +18,6 @@ WriteTsv <- function(obj, outfile, raw=TRUE) {
     cat("gene\t", paste(colnames(mat), collapse="\t"), "\n", file=outfile)
     data.table::fwrite(as.data.frame(mat), file=outfile, append=TRUE, quote=FALSE, 
         sep="\t", row.names=TRUE, col.names=FALSE)
+    if (gzip) gzip(outfile)
     return()
 }
