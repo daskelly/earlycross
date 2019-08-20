dd5e31936610e21aef63a5f13012c1236bf38a3d
Dan Skelly <daskelly@users.noreply.github.com>
2019-08-20 09:10:49 -0400

    update Write10X to work with Seurat v2 or v3 and output cellranger v2/v3

--- R/Write10X.R
+++ R/Write10X.R
@@ -1,4 +1,6 @@
 #' Write data in 10X format i.e. directory with barcodes.tsv, genes.tsv, matrix.mtx
+#' If object is Seurat v2, output cellranger2-like format
+#' If object is Seurat v3, output cellranger3-like format
 #'
 #' @param obj Seurat object to print
 #' @param dir Character. Directory in which to place 10X-like files
@@ -8,19 +10,41 @@
 #' @examples
 #' Write10X(obj, dir)
 Write10X <- function(obj, dir) {
-    assert_that(class(obj) == "seurat")
+    assert_that(class(obj) %in% c("seurat", "Seurat"))
     if (! dir.exists(dir)) {
         stop(paste0(dir, " does not exist. Stopping!"))
     }
-    
-    # Barcodes
-    cat(obj@cell.names, file = paste0(dir, "/barcodes.tsv"), sep = "\n")
-    
-    # Genes. Seurat doesn't store ENS ID. As a hack, put ENS_ID for all genes
-    df <- data.frame(ID = "ENS_ID", symbol = rownames(obj@raw.data))
-    write.table(df, row.names = F, col.names = F, sep = "\t", quote = F, file = paste0(dir, "/genes.tsv"))
-    
-    mat <- obj@raw.data[, obj@cell.names]
-    writeMM(mat, file = paste0(dir, "/matrix.mtx"))
+
+    if (class(obj) == "seurat") {
+      # Seurat v2. Will output in cellranger2-like format
+      # i.e. barcodes.tsv, genes.tsv, matrix.mtx
+      cat(obj@cell.names, file = paste0(dir, "/barcodes.tsv"), sep = "\n")
+
+      # Genes. Seurat doesn't store ENS ID. As a hack, put ENS_ID for all genes
+      df <- data.frame(ID = "ENS_ID", symbol = rownames(obj@raw.data))
+      write.table(df, row.names = F, col.names = F, sep = "\t", quote = F, file = paste0(dir, "/genes.tsv"))
+
+      mat <- obj@raw.data[, obj@cell.names]
+      writeMM(mat, file = paste0(dir, "/matrix.mtx"))
+    } else if (class(obj) == "Seurat") {
+      # Seurat v3. Will output in cellranger3-like format
+      # i.e. barcodes.tsv.gz features.tsv.gz matrix.mtx.gz
+      gz1 <- gzfile(paste0(dir, "/barcodes.tsv.gz"), "w")
+      cat(colnames(obj), file = gz1, sep = "\n")
+      close(gz1)
+
+      # Genes. Seurat doesn't store ENS ID. As a hack, put ENS_ID for all genes
+      df <- data.frame(ID = "ENS_ID", symbol = rownames(obj))
+      gz2 <- gzfile(paste0(dir, "/features.tsv.gz"), "w")
+      write.table(df, row.names = F, col.names = F, sep = "\t", quote = F, file = gz2)
+      close(gz2)
+
+      mat <- GetAssayData(obj, "counts")[, colnames(obj)]
+      gz3 <- gzfile(paste0(dir, "/matrix.mtx.gz"), "w")
+      writeMM(mat, file = gz3)
+      close(gz3)
+    } else {
+      stop("Should not get here")
+    }
     return()
 }
