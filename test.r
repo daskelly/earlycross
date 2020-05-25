65c2f06226ef5c25fed44a2e4b93452c3b389942
Dan Skelly <daskelly@users.noreply.github.com>
2020-05-25 14:49:59 -0400

    fix package name

--- test.r
+++ test.r
@@ -1,4 +1,4 @@
-library(signac)
+library(earlycross)
 pbmc_small$trt <- sample(c("drug", "control"), ncol(pbmc_small), replace=TRUE)
 pbmc_small$genotype <- sample(c("1", "2", "3"), ncol(pbmc_small), replace=TRUE)
 CompareClustersByTrt(pbmc_small, rlang::quo(trt), rlang::quo(genotype)) + ggplot2::xlab("")
