005866b59cdd6d7d831e9041223d377d64fe99e5
Dan Skelly <daskelly@users.noreply.github.com>
2020-05-26 13:05:55 -0400

    tweak WriteCELLEX to eliminate need for cellranger dir

--- R/WriteCELLEX.R
+++ R/WriteCELLEX.R
@@ -1,7 +1,6 @@
 #' Write data in CELLEX format https://github.com/perslab/CELLEX.
 #'
 #' @param seurat_obj Seurat object to print
-#' @param cellranger_dir Dir of Cellranger results. Needed to get ENS IDs
 #' @param outprefix Character. Prefix for outfiles
 #' @param outdir Character. Directory in which to place files
 #' @export
@@ -9,7 +8,7 @@
 #' @importFrom readr write_csv
 #' @examples
 #' WriteCELLEX(seurat_obj, cellranger_dir, outprefix, outdir)
-WriteCELLEX <- function(seurat_obj, cellranger_dir, outprefix, outdir = ".") {
+WriteCELLEX <- function(seurat_obj, outprefix, outdir = ".") {
     assert_that(class(seurat_obj) %in% c("seurat", "Seurat"))
     if (!dir.exists(outdir)) {
         stop(paste0(outdir, " does not exist. Stopping!"))
@@ -20,14 +19,10 @@ WriteCELLEX <- function(seurat_obj, cellranger_dir, outprefix, outdir = ".") {
         stop("Unsupported (old) Seurat object")
     } else if (class(seurat_obj) == "Seurat") {
         # Seurat v3.
-        cells <- colnames(seurat_obj)
-        warning("Assuming cellranger3 directory specified")
-        x <- CreateSeuratObject(Read10X(cellranger_dir, gene.column = 1))
-        assert_that(all(cells %in% colnames(x)))
         countsfile <- paste0(outdir, "/", outprefix, "_data.csv")
-        write_csv(as.matrix(GetAssayData(x, "counts")), path = countsfile)
+        write_csv(as.matrix(GetAssayData(seurat_obj, "counts")), path = countsfile)
         mdatfile <- paste0(outdir, "/", outprefix, "_metadata.csv")
-        mdat <- data.frame(cell_id = cells, cell_type = Idents(seurat_obj))
+        mdat <- data.frame(cell_id = colnames(seurat_obj), cell_type = Idents(seurat_obj))
         write_csv(mdat, path = mdatfile)
     } else {
         stop("Should not get here")
